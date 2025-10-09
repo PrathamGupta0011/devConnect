9906e36448c72b6252db03a1667291762a663105
PrathamGupta0011 <prathamnwb0011@gmail.com>
2025-10-09 19:39:04 +0530

    exploring get ,post, delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,43 @@
 const express = require('express');
+/**
+ *  const express = require('express');
+
+The require() function in Node.js is used to import modules.
+
+When you run this line, Node looks into the node_modules folder for a package named express.
+
+It loads the Express library and stores it in the variable express.
+
+So after this line, express becomes a function that can create an app.
+ */
 
 
 const app = express();
 
-app.use("/hello", (req, res)=>{
-    res.send("Hello this is pratham gupta");
+//this will only handle GET call to /user
+
+app.get("/user",(req,res) => {
+    res.send({firstname: "pratham", lastname:"gupta"});
+});
+
+app.post("/user",(req,res)=>{
+    //saving data to db
+    res.send("data successfully saved to the database");
 })
 
-
-app.use((req, res)=>{
-    res.send("server is listening");
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
 })
+//this will match all the http methods api calls to /test
+app.use("/test",(req,res)=>{
+    res.send("Hello from the server");
+});
 
-app.listen(3000, ()=>{
-    console.log("server is running on port ");
-})
+
+app.listen(7777, ()=>{
+    console.log("server is successfully listening on the port 7777... ");
+});
+
+/**🖥️ 6. app.listen()
+
+This tells Express to start the server and listen for incoming connections on a port. */
